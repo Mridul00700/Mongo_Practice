7465f3536563f704ca9ffaa4cb9df72e900fc709
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-12 21:00:58 +0530

    Mongo-start

---
+++ mongo.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const URL = "Your String from Mongo Atlas";
+
+
+
+const createProduct = async (req, res, next) => {
+
+}
+
+const getProducts = async (req, res, next) => {
+
+}
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
